b60e49e23a55cf374e0acbda34a3fc4ccf4653b4
unknown <rchimata@rythmos.com>
2020-05-31 19:15:04 -0500

    one-to-one association

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,28 @@
 DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS status;
+
+CREATE TABLE status (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  status_name VARCHAR(250) NOT NULL,
+);
+
+INSERT INTO status (status_name) VALUES
+  ('NEW'),
+  ('ACTIVE'),
+  ('INACTIVE');
 
 CREATE TABLE customer (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   first_name VARCHAR(250) NOT NULL,
   last_name VARCHAR(250) NOT NULL,
-  email VARCHAR(250) DEFAULT NULL
+  email VARCHAR(250) DEFAULT NULL,
+  CONSTRAINT FK_customer_status foreign key (status_id) references status(id)
 );
+
+
 
 INSERT INTO customer (first_name, last_name, email) VALUES
   ('Shanvi', 'Ch', 'shanvi@ch.com'),
   ('Kritk', 'Ch', 'kritk@ch.com'),
   ('Ram', 'Ch', 'ram@ch.com');
+
