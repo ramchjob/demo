eee86ffbfc0bec657f0df03f2a2cac62cf9cc32d
ramchjob <ramchjob@users.noreply.github.com>
2020-05-31 23:02:58 -0500

    Feature/one to one association (#2)
    * one-to-one association
    
    * Added one to one relation
    
    * One to one association
    
    * Removed lazy fetch
    
    Co-authored-by:  ram chimata

--- src/main/java/org/ram/learn/model/Status.java
+++ src/main/java/org/ram/learn/model/Status.java
@@ -3,14 +3,17 @@ package org.ram.learn.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
-@Entity(name = "status")
+@Entity
+@Table(name = "status")
 public class Status {
     
     @Id
+    @Column(name = "id")
     private Integer id;
     
     @Column(name = "status_name")
