afae11b5f22c5251d96a865072fddde72cd5f06f
ramchjob <ramchjob@users.noreply.github.com>
2020-06-06 21:33:57 -0500

    Feature/one to one association (#3)
    * one-to-one association
    
    * Added one to one relation
    
    * One to one association
    
    * Removed lazy fetch
    
    * Moved to yaml
    
    Co-authored-by: unknown <rchimata@rythmos.com>

---
+++ src/main/java/org/ram/learn/controller/OrderController.java
@@ -0,0 +1,49 @@
+package org.ram.learn.controller;
+
+import java.util.List;
+
+
+import org.ram.learn.model.Customer;
+import org.ram.learn.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    private CustomerService customerService;
+    
+    @GetMapping(produces = "application/json", value = "/customer/{orderId}")
+    private Customer getOrder(@PathVariable Integer orderId) {
+        return customerService.getCustomer(orderId);
+    }
+    
+    @PostMapping(produces = "application/json", value = "/customers")
+    private Customer createCustomer(@RequestBody  Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+    
+    @PutMapping(produces = "application/json", value = "/customer/{customerId}")
+    private Customer updateCustomer(@PathVariable Integer customerId, @RequestBody Customer customer) {
+        return customerService.updateCustomer(customerId, customer);
+    }
+    
+    @GetMapping(produces = "application/json", value = "/customers")
+    private List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+    
+    @DeleteMapping(produces = "application/json", value = "/customer/{customerId}")
+    private void deleteCustomer(@PathVariable Integer customerId) {
+        customerService.deleteCustomer(customerId);
+    }
+    
+}
---
+++ src/main/java/org/ram/learn/model/Order.java
@@ -0,0 +1,40 @@
+package org.ram.learn.model;
+
+import java.time.Instant;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "customer")
+public class Order {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    @Column(name = "order_date")
+    private Instant orderDate;
+    
+    @Column(name = "customer_id")
+    private String customerId;
+    
+    @Column(name = "priority")
+    private Boolean isPriority;
+ 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "status_id", referencedColumnName = "id")
+    private Status status; 
+}
