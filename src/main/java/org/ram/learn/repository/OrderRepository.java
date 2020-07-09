348eb6e28f0e185f242756908f6d6d4fa14ce0bd
unknown <rchimata@rythmos.com>
2020-07-09 17:10:52 -0500

    Merge conflicts

--- src/main/java/org/ram/learn/repository/OrderRepository.java
+++ src/main/java/org/ram/learn/repository/OrderRepository.java
@@ -1,10 +1,6 @@
 package org.ram.learn.repository;
 
-<<<<<<< HEAD
 import org.ram.learn.entity.Order;
-=======
-import org.ram.learn.model.Order;
->>>>>>> 8868554ae35a1ff48bc3750ecef134d230d96ca6
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Integer>{
